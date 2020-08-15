ab706aaaaf0cce67af6631f0589ba9f6ffc6d691
iqbal <asifmai@hotmail.com>
2020-08-15 19:28:39 +0500

    'update'

--- routes/index.js
+++ routes/index.js
@@ -8,25 +8,24 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET Show add profile Page */
-router.get('/addprofile', function(req, res, next) {
-  res.render('addprofile');
-});
+// router.get('/addprofile', function(req, res, next) {
+//   res.render('addprofile');
+// });
 
 /* GET Show Profiles Page */
-router.get('/profiles', indexController.profiles_get);
+// router.get('/profiles', indexController.profiles_get);
 
 /* POST add profile */
-router.post('/addprofile', indexController.addprofile_post);
+// router.post('/addprofile', indexController.addprofile_post);
 
 /* GET Details Page. */
 router.get('/details/:id', indexController.details_get);
 
 /* GET Delete Profile. */
-router.get('/deleteprofile/:id', indexController.deleteprofile_get);
+// router.get('/deleteprofile/:id', indexController.deleteprofile_get);
 
 /* POST Get Page Data. */
 router.post('/pagedata', indexController.pagedata_post);
-
 
 
 module.exports = router;
